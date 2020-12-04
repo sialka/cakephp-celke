6ee37311c44a0eae7e9f1578246b28d028d0418a
Sidnei Monteiro da Silva Filho <sialkas@gmail.com>
2020-12-04 10:09:05 -0300

    Update config route defaut

--- config/routes.php
+++ config/routes.php
@@ -61,13 +61,17 @@ Router::scope('/', function (RouteBuilder $routes) {
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
+     * 
+     * $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['prefix' => 'admin', 'controller' => 'Welcome', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
+     * 
+     * $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
      */
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    $routes->connect('/pages/*', ['prefix' => 'admin', 'controller' => 'Welcome', 'action' => 'index']);
 
     /**
      * Connect catchall routes for all controllers.
